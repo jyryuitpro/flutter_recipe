262b8e557d24c91ec4ce037623977c04c5a65d7c
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-08-08 11:46:32 +0900

    6. Container 디자인

--- lib/components/recipe_menu.dart
+++ lib/components/recipe_menu.dart
@@ -5,6 +5,53 @@ class RecipeMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _buildMenuItem(Icons.food_bank, "ALL"),
+          SizedBox(
+            width: 25,
+          ),
+          _buildMenuItem(Icons.emoji_food_beverage, "Coffee"),
+          SizedBox(
+            width: 25,
+          ),
+          _buildMenuItem(Icons.fastfood, "Bugger"),
+          SizedBox(
+            width: 25,
+          ),
+          _buildMenuItem(Icons.local_pizza, "Pizza"),
+        ],
+      ),
+    );
+  }
+
+  Container _buildMenuItem(IconData mIcon, String text) {
+    return Container(
+      width: 60,
+      height: 80,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black12,
+        ),
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            mIcon,
+            color: Colors.redAccent,
+            size: 30,
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(text),
+        ],
+      ),
+    );
   }
 }
